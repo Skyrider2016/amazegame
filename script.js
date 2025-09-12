9813f4565f82da1c66cec622e07079c5f7601acf
Skyrider2016 <lucasborgesmaia@gmail.com>
2025-09-11 22:32:44 -0300

    Iniciativa Dados

--- script.js
+++ script.js
@@ -241,7 +241,7 @@ function rolarTodasIniciativas() {
 
 function rolarDadoPorTier(nome, tier) {
   
-  const dados = { S: 2, A: 4, B: 6, C: 8, D: 10 };
+  const dados = { S: 4, A: 6, B: 8, C: 10, D: 12 };
   const max = dados[tier] || 6;
   let result = 0
   let dadoText = nome +` rolou 3d${max}(`
